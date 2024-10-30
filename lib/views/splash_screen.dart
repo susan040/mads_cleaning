77b349c9a11123bd6a53303e566e0c9f377abeee
susan040 <susanthapa040@gmail.com>
2024-10-30 11:00:56 +0545

    completed splash screen

---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mads_cleaning/controller/splash_screen_controller.dart';
+import 'package:mads_cleaning/utils/colors.dart';
+import 'package:mads_cleaning/utils/image_path.dart';
+
+class SplashScreen extends StatelessWidget {
+  final c = Get.put(SplashScreenController());
+  SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColors.extraWhite,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                ImagePath.logo,
+                height: 195,
+                width: 195,
+              ),
+              const SizedBox(height: 45),
+              const CircularProgressIndicator(
+                backgroundColor: AppColors.secondaryColor,
+                valueColor: AlwaysStoppedAnimation(AppColors.primaryColor),
+              ),
+              const SizedBox(height: 60),
+            ],
+          ),
+        ));
+  }
+}
